2bc42a39099b72e4e52aa1df1f460670f508ec43
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-04-22 23:47:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_213318) do
-
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-    t.integer "song_ids"
-    t.integer "genre_ids"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
