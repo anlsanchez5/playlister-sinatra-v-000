11ce91ca8a3c3b5b65f7f5de180c8084213fa416
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-04-22 23:46:11 +0000

    Automatically backed up by Learn

--- db/migrate/20190412213318_create_artists.rb
+++ db/migrate/20190412213318_create_artists.rb
@@ -2,8 +2,6 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.string :name
-      t.integer :song_ids
-      t.integer :genre_ids
     end
   end
 end
