494b060b5381f53d3bacad1529312254d1c61bcd
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-04-22 23:33:59 +0000

    Automatically backed up by Learn

--- db/migrate/20190422225303_rename_song_genres_to_genres_songs.rb
+++ db/migrate/20190422225303_rename_song_genres_to_genres_songs.rb
@@ -1,5 +1,5 @@
 class RenameSongGenresToGenresSongs < ActiveRecord::Migration[5.2]
   def change
-    rename_table :song_genres, :genres_songs
+    rename_table :song_genres, :songs_genres
   end
 end
